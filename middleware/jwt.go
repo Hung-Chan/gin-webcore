fb06bc6cd1570d2234a9af499238c1712dc7df6a
阿fa <20190303@cyadmk.com>
2019-12-31 17:43:35 +0800

    [add]添加jwt-middleware

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -1,22 +1,43 @@
 package middleware
 
 import (
+	"gin-webcore/message"
+	"gin-webcore/response"
+	"gin-webcore/utils"
 	"time"
 
-	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
 )
 
-var secret = []byte("secret")
+// Jwt .
+func Jwt() gin.HandlerFunc {
+	return func(context *gin.Context) {
 
-// GenerateToken .
-func GenerateToken(account string) (string, error) {
+		var code int = message.Success
 
-	claims := jwt.MapClaims{
-		"account": account,
-		"exp":     time.Now().Add(time.Hour * 2).Unix(),
+		response := response.Gin{Context: context}
+		// token .
+		token := context.Query("token")
+
+		if token != "" {
+			code = message.TokenEmptyString
+		} else {
+			claims, err := utils.ParseToken(token)
+
+			if err != nil {
+				code = message.TokenParseError
+			} else if time.Now().Unix() > claims.ExpiresAt {
+				code = message.TokenTimeout
+			}
+		}
+
+		if code != message.Success {
+			response.ResultError(code)
+
+			context.Abort()
+			return
+		}
+
+		context.Next()
 	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-
-	return token.SignedString(secret)
 }
