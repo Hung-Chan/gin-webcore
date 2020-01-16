60081b76c1bc3d73bcbafe855342029ce5badfba
阿fa <20190303@cyadmk.com>
2020-01-16 15:27:08 +0800

    [mod]移除舊的QueryModel

--- models/publicmodel.go
+++ models/publicmodel.go
@@ -4,16 +4,6 @@ import "time"
 
 // QueryModel : 此Model為所有查詢功能固定所帶參數 .
 type QueryModel struct {
-	Page          int    `form:"page"`
-	Limit         int    `form:"limit"`
-	SortColumn    string `form:"sortColumn"`
-	SortDirection string `form:"sortDirection"`
-	Enable        int    `form:"enable"`
-	Name          string `form:"name"`
-}
-
-// QueryModelNew : 此Model為所有查詢功能固定所帶參數 .
-type QueryModelNew struct {
 	Page          int     `form:"page" example:"1"`
 	Limit         int     `form:"limit" example:"10"`
 	SortColumn    string  `form:"sortColumn" example:"id"`
@@ -37,8 +27,8 @@ type IDInfo struct {
 }
 
 // NewQueryModel .
-func NewQueryModel() *QueryModelNew {
-	return &QueryModelNew{
+func NewQueryModel() *QueryModel {
+	return &QueryModel{
 		Page:          1,
 		Limit:         10,
 		SortColumn:    "id",
